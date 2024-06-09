43b6a81e2a780adf51ea7dcbf0029a951c3e4ac8
MahidunNobi <mdmahidunnobi@gmail.com>
2024-06-09 11:58:25 +0600

    Initial setup with mongoDB and user sign and jwt api created

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+
+// middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Post pulse server is going on here....");
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("post-portal").collection("users");
+
+    // jwt related token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: "1hr",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ succcess: true });
+    });
+
+    // User related api's
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.json({ message: "User already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne({
+        ...user,
+        timestamp: Date.now(),
+      });
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server running of port ${port}`);
+});
